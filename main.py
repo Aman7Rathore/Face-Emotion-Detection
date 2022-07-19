d2a28206099296719375ee8008189e075c3d2181
Aman <62935266+Aman7Rathore@users.noreply.github.com>
2022-07-20 00:04:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+import streamlit as st
+import cv2
+from PIL import Image
+import numpy as np
+import os
 
 from keras.models import load_model
 from time import sleep
@@ -47,3 +51,37 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
+def main():
+    st.title("Face Detection App :sunglasses: ")
+    st.write("**Using the Haar cascade Classifiers**")
+
+    activities = ["Home", "About"]
+    choice = st.sidebar.selectbox("Pick something fun", activities)
+
+    if choice == "Home":
+
+        st.write("Go to the About section from the sidebar to learn more about it.")
+        
+        # You can specify more file types below if you want
+        image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+
+        if image_file is not None:
+
+            image = Image.open(image_file)
+
+            if st.button("Process"):
+                
+                # result_img is the image with rectangle drawn on it (in case there are faces detected)
+                # result_faces is the array with co-ordinates of bounding box(es)
+                result_img, result_faces = detect(image=image)
+                st.image(result_img, use_column_width = True)
+                st.success("Found {} faces\n".format(len(result_faces)))
+
+    elif choice == "About":
+        about()
+
+
+
+
+if __name__ == "__main__":
+    main()
